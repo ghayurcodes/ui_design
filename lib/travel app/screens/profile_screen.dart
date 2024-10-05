21aada554e13e28e46aab67b07c9dc87ed7788ab
Ghayur <154737827+ghayurcodes@users.noreply.github.com>
2024-10-05 19:31:29 +0500

    Update profile_screen.dart

--- lib/travel app/screens/profile_screen.dart
+++ lib/travel app/screens/profile_screen.dart
@@ -5,6 +5,10 @@ class profile extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+
+    var _width = MediaQuery.of(context).size.width;
+    var _height = MediaQuery.of(context).size.height;
+
     return SafeArea(
       child: Container(
           color: Colors.grey,
@@ -14,6 +18,19 @@ class profile extends StatelessWidget {
                 flex: 4,
                 child: Container(
                   color: Colors.orange,
+                  child: Stack(
+                    children: [
+                      Container(
+                        color: Colors.red,
+                      ),
+                      Positioned(
+                        child: Container(
+                          color:Colors.pinkAccent,
+                          height: _height*0.1,
+                        ),
+                      )
+                    ],
+                  ),
 
                 ),
               ),
