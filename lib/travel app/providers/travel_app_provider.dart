c5756d20c8d9a0e447cb27eea5e8ee0afddd1785
Ghayur <154737827+ghayurcodes@users.noreply.github.com>
2024-10-06 20:39:05 +0500

    Update travel_app_provider.dart

--- lib/travel app/providers/travel_app_provider.dart
+++ lib/travel app/providers/travel_app_provider.dart
@@ -23,18 +23,19 @@ class home_provider with ChangeNotifier {
   var options=["Most Viewed","Nearby","Latest","Popular","Adventure"];
   var wonder_images=["https://www.weirdosabroad.com/wp-content/uploads/2018/10/christ-the-redeemer-corcovado-brazil-e1538568170179.jpg",
   "https://socialstudieshelp.com/wp-content/uploads/2024/03/The-Great-Wall-of-China-Beyond-the-Bricks.webp"
-  ,""];
+  ,"https://www.thoughtco.com/thmb/mvzDYqXzP4T_D7JfMTsWm5GgDZA=/1500x0/filters:no_upscale():max_bytes(150000):strip_icc()/sunrise-at-taj-mahal--agra--uttar-pradash--india-583682538-5b91840bc9e77c0050bdc67b.jpg"];
 
 
-  var wondername1=["CHRIST THE REDEEMER","Great Wall"];
+  var wondername1=["CHRIST THE REDEEMER","Great Wall","Taj Mahal","Petra"];
 
 
-  var wondername2=["Brazil","China"];
+  var wondername2=["Brazil","China","India","Jordan",];
 
 
   var wonderdesc=["In Portuguese, Christ the Redeemer is also known as the Cristo Redentor. It is a massive statue of Jesus Christ that stands atop Mount Corcovado in Rio de Janeiro, Brazil. Corcovado rises over Rio de Janeiro, Brazil’s Brazil’s main port city, immortalised in traditional and popular music. The Christ the Redeemer monument, which rises 98 feet (30 metres) tall with 92 feet of horizontal spread arms, was finished in 1931. (28 metres). The monument has become a symbol for both Rio de Janeiro and the whole country of Brazil.",
   "The Great Wall of China is a chain of fortifications constructed of stone, brick, compressed earth, wooden, and other resources that run east-west across China’s historical northern borders to protect Chinese states and empires against assaults and invasions by various nomadic peoples from the Eurasian Steppe\nBorder restrictions, allowing the application of tariffs on products conveyed along the Silk Route, trade restrictions and stimulation, and high migration controls are among the other uses of the Great Wall in China.",
-    ""];
+    "Mughal emperor Shah Jahan erected the Taj Mahal to house the remains of his beloved wife, Mumtaz Mahal. In Agra, India, the Taj Mahal was originally constructed over 20 years, mostly on the Yamuna River’s River’s southern bank, and is one of the most famous examples of Mughal architecture, which blended Indian, Persian, and Islamic components.",""
+        ""];
 
 
   int get selectedindex => _selectedIndex;
