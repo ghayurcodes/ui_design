0717172286c26568bb6ea1cab564d54f6c1fec3b
Ghayur <154737827+ghayurcodes@users.noreply.github.com>
2024-10-06 20:09:54 +0500

    update

--- lib/travel app/providers/travel_app_provider.dart
+++ lib/travel app/providers/travel_app_provider.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
+import 'package:http/http.dart';
 
 import '../screens/explore.dart';
 import '../screens/profile_screen.dart';
