976722364997e5ed588096f6262b6d1a8b3305a1
Ghayur <154737827+ghayurcodes@users.noreply.github.com>
2024-10-13 19:32:48 +0500

    Update likes_screen.dart

--- lib/travel app/screens/likes_screen.dart
+++ lib/travel app/screens/likes_screen.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:ui_design/travel%20app/providers/travel_app_provider.dart';
 
 class likes_screen extends StatefulWidget {
   const likes_screen({super.key});
@@ -12,7 +15,20 @@ class _likes_screenState extends State<likes_screen> {
   Widget build(BuildContext context) {
     return SafeArea(
       child: Container(
-      child: Text("wefwefdrgergeg"),
+      color: Colors.white,
+        child: Consumer<home_provider>(builder: (context, value, child) {
+          return ListView.builder(itemBuilder:  (context, index) {
+            return value.favs.contains(index)?Container(
+              color: Colors.yellowAccent,
+              width: 200,
+              height: 100,
+            ):Container(
+              color: Colors.pinkAccent,
+              width: 200,
+              height: 40,
+            );
+          },itemCount: value.wondername1.length,);
+        },),
       ),
     );
   }
