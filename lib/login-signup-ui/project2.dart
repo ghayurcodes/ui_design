ab258798b9dcacec6dcf2c8bade4f4ad81e6b373
Ghayur <154737827+ghayurcodes@users.noreply.github.com>
2024-10-23 19:33:12 +0500

    Update project2.dart

--- lib/login-signup-ui/project2.dart
+++ lib/login-signup-ui/project2.dart
@@ -1,49 +1,81 @@
 import 'package:flutter/material.dart';
 
 class login extends StatelessWidget {
-  const login ({super.key});
+  const login({super.key});
 
   @override
   Widget build(BuildContext context) {
-    var _width=MediaQuery.of(context).size.width;
-    var _height=MediaQuery.of(context).size.height;
+    var _width = MediaQuery.of(context).size.width;
+    var _height = MediaQuery.of(context).size.height;
 
     return Scaffold(
       backgroundColor: Colors.black,
-      resizeToAvoidBottomInset: false ,
-    body: Center(
-    child: Container(
-        width:_width,
-        height: _height,
-        child: Column(
-          children: [
-            Expanded(
-              flex: 4,
-                child: Container(
-                  color: Colors.red,
-            )),
-            Expanded(
-              flex: 7,
-                child: Column(mainAxisAlignment: MainAxisAlignment.start,
-              crossAxisAlignment: CrossAxisAlignment.start,
+      resizeToAvoidBottomInset: false,
+      body: Center(
+        child: Container(
+            width: _width,
+            height: _height,
+            child: Column(
               children: [
-                Text("Login",style: TextStyle(
-                  fontSize: _width*0.1,
-                  color: Colors.white
-                ),),
-                Text('Email',style: TextStyle(
-                  color: Colors.white
-                ),),
-                custom_feild(),
+                Expanded(
+                    flex: 4,
+                    child: Container(
+                      color: Colors.red,
+                    )),
+                Expanded(
+                    flex: 7,
+                    child: Padding(
+                      padding: const EdgeInsets.symmetric(
+                          horizontal: 25, vertical: 15),
+                      child: Column(
+                        mainAxisAlignment: MainAxisAlignment.start,
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          Text(
+                            "Login",
+                            style: TextStyle(
+                                fontSize: _width * 0.1, color: Colors.white),
+                          ),
+                          SizedBox(
+                            height: _width * 0.01,
+                          ),
+                          Padding(
+                            padding: const EdgeInsets.all(8.0),
+                            child: Text(
+                              'Email',
+                              style: TextStyle(color: Colors.white),
+                            ),
+                          ),
+                          custom_feild(),
+                          SizedBox(
+                            height: _width * 0.04,
+                          ),
+                          Padding(
+                            padding: const EdgeInsets.all(8.0),
+                            child: Text(
+                              'Password',
+                              style: TextStyle(color: Colors.white),
+                            ),
+                          ),
+                          custom_feild(),
+                          Padding(
+                            padding: const EdgeInsets.all(8.0),
+                            child: Align(
+                              alignment: Alignment.centerRight,
+                              child: Text(
+                                'Forgot Password',
+                                style: TextStyle(color: Colors.grey),
+                              ),
+                            ),
+                          ),
+                          
 
-                
+                        ],
+                      ),
+                    ))
               ],
-            ))
-
-          ],
-        )
-),
-    ),
+            )),
+      ),
     );
   }
 }
@@ -51,7 +83,16 @@ class login extends StatelessWidget {
 
 
 
-custom_feild(){
+
+
+
+
+
+
+
+
+
+custom_feild() {
   return TextField(
     cursorColor: Colors.white,
     decoration: InputDecoration(
@@ -59,13 +100,13 @@ custom_feild(){
       contentPadding: EdgeInsets.all(5),
       enabledBorder: OutlineInputBorder(
         borderRadius: BorderRadius.circular(25),
-        borderSide: BorderSide(
-            color: Colors.white),
+        borderSide: BorderSide(color: Colors.grey),
       ),
       focusedBorder: OutlineInputBorder(
         borderRadius: BorderRadius.circular(25),
         borderSide: BorderSide(
-            color: Colors.white,),
+          color: Colors.grey,
+        ),
       ),
     ),
     maxLines: 1,
