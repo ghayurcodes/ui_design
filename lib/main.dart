ae6485846aa1cc1b594988933286db17fce4fd5b
Ghayur <154737827+ghayurcodes@users.noreply.github.com>
2024-11-13 20:07:32 +0500

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -25,7 +25,7 @@ class MyApp extends StatelessWidget {
       ],
       child: MaterialApp(
         debugShowCheckedModeBanner: false,
-        home: temo(),
+        home: temo()
       ),
     );
   }
