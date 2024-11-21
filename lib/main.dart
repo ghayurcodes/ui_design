c516d034da316143ca6b92ee2409c3256840bbca
Ghayur <154737827+ghayurcodes@users.noreply.github.com>
2024-11-21 23:41:44 +0500

    up

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,8 @@
 import 'dart:math';
+import 'package:hive/hive.dart';
 
 import 'package:flutter/material.dart';
+import 'package:hive_flutter/hive_flutter.dart';
 import 'package:ui_design/login-signup-ui/sugnup_screen.dart';
 import 'package:ui_design/money%20split/home.dart';
 import 'package:ui_design/travel%20app/providers/travel_app_provider.dart';
@@ -9,7 +11,10 @@ import 'package:ui_design/travel%20app/screens/spalsh_screen.dart';
 import 'package:provider/provider.dart';
 
 import 'login-signup-ui/login_screen.dart';
-void main() {
+void main() async {
+  await Hive.initFlutter();
+  
+  var box=Hive.openBox("MoneySplit");
   runApp(const MyApp());
 }
 
--- lib/money split/home.dart
+++ lib/money split/home.dart
@@ -3,6 +3,7 @@ import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
 import 'package:google_fonts/google_fonts.dart';
+import 'package:hive/hive.dart';
 
 class homepage extends StatelessWidget {
    homepage({super.key});
