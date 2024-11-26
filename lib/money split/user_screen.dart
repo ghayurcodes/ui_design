024e1bf120114efcb9185df3e59b78860d2f1084
Ghayur <154737827+ghayurcodes@users.noreply.github.com>
2024-11-26 21:14:54 +0500

    Update user_screen.dart

--- lib/money split/user_screen.dart
+++ lib/money split/user_screen.dart
@@ -1,9 +1,11 @@
 
+import 'package:flutter/material.dart';
+
 class user_screen extends StatelessWidget {
   const user_screen({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold();
   }
 }
