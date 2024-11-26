53828f048ee79a0f4c1e08a82dd4bd6e25cf70c5
Ghayur <154737827+ghayurcodes@users.noreply.github.com>
2024-11-26 22:32:26 +0500

    Update user_screen.dart

--- lib/money split/user_screen.dart
+++ lib/money split/user_screen.dart
@@ -1,88 +1,113 @@
-
 import 'package:flutter/material.dart';
-import 'package:line_icons/line_icons.dart';
 
 class user_screen extends StatelessWidget {
   const user_screen({super.key});
 
   @override
   Widget build(BuildContext context) {
-
-    var _height=MediaQuery.of(context).size.height;
-    var _width=MediaQuery.of(context).size.width;
+    var _height = MediaQuery.of(context).size.height;
+    var _width = MediaQuery.of(context).size.width;
 
     return Scaffold(
       appBar: AppBar(
         backgroundColor: Colors.white,
-        actions: [Padding(
-          padding: const EdgeInsets.all(8.0),
-          child: Icon(Icons.more_vert_rounded),
-        )],
+        actions: [
+          Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: Icon(Icons.more_vert_rounded),
+          )
+        ],
       ),
       body: Container(
         width: _width,
         height: _height,
-          color: Colors.grey,
+        color: Colors.grey,
         child: Center(
           child: Column(
             children: [
               Expanded(
                 child: Column(
                   children: [
-                    Expanded(child: Container(
-                      color: Colors.lightBlueAccent,
-                      // margin: EdgeInsets.all(15),
-                      child: Row(
-                        mainAxisAlignment: MainAxisAlignment.spaceAround,
-                        children: [
-                          Container(
-                            decoration: BoxDecoration(
-                              shape: BoxShape.circle,
-                              color: Colors.black,
+                    Expanded(
+                      flex: 3,
+                      child: Container(
+                        color: Colors.lightBlueAccent,
+                        child: Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceAround,
+                          children: [
+                            Container(
+                              decoration: BoxDecoration(
+                                shape: BoxShape.circle,
+                                color: Colors.black,
+                              ),
+                              padding: EdgeInsets.all(3),
+                              child: CircleAvatar(
+                                backgroundColor: Colors.redAccent,
+                                radius: 60,
+                              ),
                             ),
-                
-                            padding: EdgeInsets.all(3),
-                            child: CircleAvatar(
-                              backgroundColor: Colors.redAccent,
-                              radius: 60,
-                            ),
-                          ),
-                          FittedBox(
-                            child: Column(
-                              children: [
-                                Text("Nmae Here",style: TextStyle(fontSize: _width*0.1),
-                                ),
-                                Text("you owe",style: TextStyle(fontSize: _width*0.05,color: Colors.red),)
-                              ],
-                            ),
-                          )
-                        ],
+                            Flexible(
+                              child: Column(
+                                mainAxisAlignment: MainAxisAlignment.center,
+                                children: [
+                                  FittedBox(
+                                    fit: BoxFit.scaleDown,
+                                    child: Text(
+                                      "Name dddddddd",
+                                      style: TextStyle(
+                                        fontSize: _width * 0.1,
+                                        fontWeight: FontWeight.bold,
+                                      ),
+                                      textAlign: TextAlign.center,
+                                    ),
+                                  ),
+                                  SizedBox(height: 5),
+                                  Text(
+                                    "you owe",
+                                    style: TextStyle(
+                                      fontSize: _width * 0.05,
+                                      color: Colors.red,
+                                    ),
+                                  ),
+                                ],
+                              ),
+                            )
+                          ],
+                        ),
                       ),
-                    ),flex: 3,),
-                    Expanded(child: Container(
-                      color: Colors.yellow,
-                    ),flex: 7,),
+                    ),
+                    Expanded(
+                      flex: 7,
+                      child: Container(
+                        color: Colors.yellow,
+                      ),
+                    ),
                   ],
                 ),
               ),
               Container(
-                margin: EdgeInsets.only(bottom: 10,right: 10,left: 10),
+                margin: EdgeInsets.only(bottom: 10, right: 10, left: 10),
                 width: _width,
-                height: _height*0.1,
+                height: _height * 0.1,
                 decoration: BoxDecoration(
-                    color: Colors.white,
-                    borderRadius: BorderRadius.circular(25)
+                  color: Colors.white,
+                  borderRadius: BorderRadius.circular(25),
                 ),
                 child: Row(
                   mainAxisAlignment: MainAxisAlignment.spaceAround,
                   children: [
-                    Icon(Icons.history,size: _width*0.1,),
-                    InkWell(child: FittedBox(child: Icon(Icons.add_circle_rounded,size: _width*0.2,color: Colors.green,)),
-                      onTap: (){},),
-
-                    Icon(Icons.person,size: _width*0.1,),
-
-
+                    Icon(Icons.history, size: _width * 0.1),
+                    InkWell(
+                      child: FittedBox(
+                        child: Icon(
+                          Icons.add_circle_rounded,
+                          size: _width * 0.2,
+                          color: Colors.green,
+                        ),
+                      ),
+                      onTap: () {},
+                    ),
+                    Icon(Icons.person, size: _width * 0.1),
                   ],
                 ),
               )
